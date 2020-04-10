e9e83c25863e36ae8c38dad1726fecda2781340d
justinchoyy <justin.choy.2017@sis.smu.edu.sg>
2020-04-10 12:29:55 +0800

    edited app

--- project_dashboard/app.R
+++ project_dashboard/app.R
@@ -1,3 +1,9 @@
+#Contents:
+#    1. Package Imports
+#    2. Loading of data
+#    3. Visualisation Functions
+#    4. Application UI
+
 #Package
 
 library(shiny)
@@ -6,6 +12,12 @@ library(plotly)
 library(ggplot2)
 library(viridis)
 library(GGally)
+
+
+# Loading of Data:
+residential_data_2019 <- read_csv("Data/Residential_Planning/respopagesextod2011to2019.csv")
+
+ls()
 
 
 #UI Section
